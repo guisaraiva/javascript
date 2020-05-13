4aa8e1716c38a266b5b9cc4c3160239dda5663c8
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-05-12 22:41:32 -0300

    Iniciando aprendizagem com node - JavaScript

---
+++ Node/moduloA.js
@@ -0,0 +1,5 @@
+//Em node um arquivo corresponde há um módulo.
+this.ola = 'Fala pessoal'
+exports.bemVindo = 'Bem vindo ao Node.'
+module.exports.ateLogo = 'Até o próximo exemplo.'
+
---
+++ Node/moduloB.js
@@ -0,0 +1,5 @@
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite (){
+        return  'Boa noite'   } 
+}
